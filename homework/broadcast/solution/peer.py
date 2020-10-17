ccae17c339ccb753d140fd33ede75d809c20ea03
murfel <murfel@users.noreply.github.com>
2020-10-17 20:10:55 +0300

    add solution

--- homework/broadcast/solution/peer.py
+++ homework/broadcast/solution/peer.py
@@ -2,7 +2,9 @@
 
 import argparse
 import logging
+import json
 
+from collections import defaultdict
 from dslib import Communicator, Message
 
 
@@ -11,7 +13,17 @@ class Peer:
         self._name = name
         self._peers = peers
         self._comm = Communicator(name, addr)
-    
+
+        self.num_msg_sent = 0
+        self.user_to_id_to_msg = defaultdict(dict)
+        self.user_to_num_delivered = defaultdict(int)
+
+        self.received = defaultdict(int)
+
+    def deliver_local(self, msg):
+        deliver_msg = Message('DELIVER', json.loads(msg.headers)['from'] + ': ' + msg.body)
+        self._comm.send_local(deliver_msg)
+
     def run(self):
         while True:
             msg = self._comm.recv()
@@ -19,15 +31,45 @@ class Peer:
             # local user wants to send a message to the chat
             if msg.type == 'SEND' and msg.is_local():
                 # basic broadcast
-                bcast_msg = Message('BCAST', msg.body, {'from': self._name})
+                bcast_msg = Message('BCAST', msg.body, json.dumps({'from': self._name, 'msg_id': self.num_msg_sent}))
+                self.num_msg_sent += 1
                 for peer in self._peers:
                     self._comm.send(bcast_msg, peer)
 
             # received broadcasted message
             elif msg.type == 'BCAST':
-                # deliver message to the local user
-                deliver_msg = Message('DELIVER', msg.headers['from'] + ': ' + msg.body)
-                self._comm.send_local(deliver_msg)
+                headers = json.loads(msg.headers)
+                user = headers['from']
+                msg_id = headers['msg_id']
+
+                if user != self._name:
+                    if msg not in self.received.keys():
+                        for peer in self._peers:
+                            self._comm.send(msg, peer)
+                if user == self._name:
+                    if msg not in self.received.keys():
+                        self.received[msg] -= 1
+
+                self.received[msg] += 1
+
+                if self.received[msg] == int(len(self._peers) / 2):
+                    msg_id_to_msg = self.user_to_id_to_msg[user]
+                    msg_id_to_msg[msg_id] = msg
+
+                    if self.user_to_num_delivered[user] == msg_id:
+                        while msg_id in msg_id_to_msg:
+                            self.deliver_local(msg_id_to_msg[msg_id])
+                            self.user_to_num_delivered[user] += 1
+                            msg_id += 1
+
+                # if msg in self.received:
+                #     continue
+                # headers = json.loads(msg.headers)
+                # self.received.add(msg)
+                # if headers['from'] != self._name:
+                #     for peer in self._peers:
+                #         self._comm.send(msg, peer)
+                # self.deliver_local(msg)
 
 
 def main():
